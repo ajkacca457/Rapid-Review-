0bcccd010bd3d0a9bc442fe96a53517625e176b3
Avijit Karmaker <ajkacca@gmail.com>
2021-10-24 20:23:43 +0300

    add middleware in review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const slugify=require("slugify");
 
 
 const ReviewSchema= new mongoose.Schema({
@@ -39,7 +40,10 @@ createdAt : {
 
 });
 
-
+ReviewSchema.pre("save",function(next){
+this.slug= slugify(this.title,{lower:true})
+next()
+})
 
 
 module.exports= mongoose.model("Reviews", ReviewSchema); 
