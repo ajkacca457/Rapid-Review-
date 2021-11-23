b86b3cbe3d28b8b64b9349fc9ca449428914201c
Avijit Karmaker <ajkacca@gmail.com>
2021-11-23 23:44:04 +0200

    add protection middleware for review routes

--- router/reviews.js
+++ router/reviews.js
@@ -2,17 +2,17 @@ const express= require("express");
 const router= express.Router();
 const advancedResults= require("../middlewares/advancedResults");
 const Reviews= require("../models/reviewModel");
-
+const { protect }= require("../middlewares/auth");
 const {getReviews, postReview,getReview,updateReview,deleteReview} = require("../controllers/reviewsController");
 
 router.route("/")
 .get(advancedResults(Reviews),getReviews)
-.post(postReview)
+.post(protect,postReview)
 
 router.route("/:id")
 .get(getReview)
-.put(updateReview)
-.delete(deleteReview)
+.put(protect,updateReview)
+.delete(protect,deleteReview)
 
 
 module.exports= router;
